d4d760c928e61de98c179ae14c117f02c8470295
ansuzcoder <eugene.bevz06@gmail.com>
2024-09-12 16:49:36 +0300

    initial commit

---
+++ src/FibonacciNumber.java
@@ -0,0 +1,42 @@
+public class FibonacciNumber {
+    private final int numberPosition;
+    private final int numberValue;
+
+    /**
+     * class builder
+     *
+     * @param newNumberPosition position in the array
+     * @param newNumberValue    number value
+     */
+    public FibonacciNumber(
+            int newNumberPosition,
+            int newNumberValue
+    ) {
+        this.numberPosition = newNumberPosition;
+        this.numberValue = newNumberValue;
+    }
+
+    /**
+     * number position getter
+     *
+     * @return int
+     */
+    public int getNumberPosition() { return numberPosition; }
+
+    /**
+     * number value getter
+     *
+     * @return int
+     */
+    public int getNumberValue() { return numberValue; }
+
+    /**
+     * determine whether a number is a perfect square
+     *
+     * @return boolean
+     */
+    public boolean isSquare() {
+        int intVal = (int) Math.sqrt(numberValue);
+        return intVal * intVal == numberValue;
+    }
+}
